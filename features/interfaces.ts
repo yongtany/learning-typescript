6223ffea8f70361158a739a94c4aa2a44e3c9775
yongtany <yongn1494@gmail.com>
2019-08-10 15:44:07 +0900

    Functions in Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary(): string;
 }
 
@@ -14,11 +11,8 @@ const oldCivic = {
   }
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  // console.log(`Name: ${vehicle.name}`);
-  // console.log(`Year: ${vehicle.year}`);
-  // console.log(`Broken? ${vehicle.broken}`);
-  console.log(vehicle.summary());
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
