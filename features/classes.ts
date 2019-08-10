6f167fd372003e6fd70fd2ecade020200218618b
yongtany <yongn1494@gmail.com>
2019-08-10 16:52:10 +0900

    Fields in Classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,8 +1,13 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
 class MotoCycle extends Vehicle {
   private drive(): void {
