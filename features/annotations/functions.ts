e08d43d88b299145a119cf5108666692d7f25ae4
yongtany <yongn1494@gmail.com>
2019-08-10 13:32:46 +0900

    Annotations for Anonymous Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -5,3 +5,11 @@ const add = (a: number, b: number): number => {
 const substract = (a: number, b: number) => {
   return a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
