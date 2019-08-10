f23b1aea4eb2e5756e72d2e85948b3821befd870
yongtany <yongn1494@gmail.com>
2019-08-10 14:07:36 +0900

    Multiple Types in Arrays

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,4 +1,23 @@
 const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
-const carsByMake = [['f150'], ['corolla'], ['camaro']];
+const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+//const car: string = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+//carMakers.push(100);
+
+// Help with 'map'
+carMakers.map(
+  (car: string): string => {
+    return car.toUpperCase();
+  }
+);
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('2030-10-11');
+importantDates.push(new Date());
