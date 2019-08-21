0dae209aaf6805dcce7ddef88ad0940779182c43
yongtany <yongn1494@gmail.com>
2019-08-21 19:02:46 +0900

    Adding Marker

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,8 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
