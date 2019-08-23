ab93376d64c78aa14b4189a9e1267429e473c37a
yongtany <yongn1494@gmail.com>
2019-08-23 16:03:58 +0900

    Solving All Issues with Abstract Classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,19 +1,14 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// console.log(sorter.collection);
-// sorter.sort();
-// console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('yXngtanY');
-// const sorter = new Sorter(charactersCollection);
-// console.log(sorter.collection);
-// sorter.sort();
-// console.log(sorter.collection);
+const charactersCollection = new CharactersCollection('yXngtanY');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -21,6 +16,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
