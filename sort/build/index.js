9ec2342ad27605059601fa0ff5b6651f28422736
yongtany <yongn1494@gmail.com>
2019-08-23 12:42:14 +0900

    Sorting Aribitrary Collections

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,8 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('yXngtanY');
 var sorter = new Sorter_1.Sorter(numbersCollection);
+var sorter2 = new Sorter_1.Sorter(charactersCollection);
 console.log(sorter.collection);
 sorter.sort();
 console.log(sorter.collection);
+console.log('==============');
+console.log(sorter2.collection);
+sorter2.sort();
+console.log(sorter2.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('yXngtanY');
+
 const sorter = new Sorter(numbersCollection);
+const sorter2 = new Sorter(charactersCollection);
+
 console.log(sorter.collection);
 sorter.sort();
 console.log(sorter.collection);
+console.log('==============');
+console.log(sorter2.collection);
+sorter2.sort();
+console.log(sorter2.collection);
