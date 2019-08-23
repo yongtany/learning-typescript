7101eafe5cd6f38b0f10dc7657c0a63f7b822ac2
yongtany <yongn1494@gmail.com>
2019-08-23 15:34:33 +0900

    Completed Linked List Code

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,17 +1,26 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const charactersCollection = new CharactersCollection('yXngtanY');
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// console.log(sorter.collection);
+// sorter.sort();
+// console.log(sorter.collection);
 
-const sorter = new Sorter(numbersCollection);
-const sorter2 = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('yXngtanY');
+// const sorter = new Sorter(charactersCollection);
+// console.log(sorter.collection);
+// sorter.sort();
+// console.log(sorter.collection);
 
-console.log(sorter.collection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(sorter.collection);
-console.log('==============');
-console.log(sorter2.collection);
-sorter2.sort();
-console.log(sorter2.collection);
+linkedList.print();
