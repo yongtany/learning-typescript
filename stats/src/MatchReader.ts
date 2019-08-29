fca55e06f45607686651f2e1807c1a9cc866befc
yongtany <yongn1494@gmail.com>
2019-08-29 16:10:12 +0900

    Extracting Match Reference

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,3 +1,8 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
   read(): void;
   data: string[][];
