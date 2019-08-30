f02471289c00db0503d38ca96a36bec0cdfb3fc9
yongtany <yongn1494@gmail.com>
2019-08-30 13:57:49 +0900

    Putting It All Together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -9,4 +11,10 @@ const csvFileReader = new CsvFileReader('football.csv');
 // the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-// matchReader.matches
+
+const summary = new Summary(
+  new WinsAnalysis(`Man United`),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
