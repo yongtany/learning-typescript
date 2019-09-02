29b97d064ee362aae7247dfd871852fe1cf3c903
yongtany <yongn1494@gmail.com>
2019-09-02 21:12:51 +0900

    Checking Login Status

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -35,4 +35,22 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 });
 
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Logout</a>
+      </div>
+    `);
+  }
+});
+
 export { router };
