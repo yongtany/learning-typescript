3a48d6e7fe90671eb7b64884c7b01bbc4b314b30
yongtany <yongn1494@gmail.com>
2019-12-17 15:10:28 +0900

    Functional Component

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -5,30 +5,38 @@ interface AppProps {
   color?: string;
 }
 
-class App extends React.Component<AppProps> {
-  state ={ 
-    counter: 0
-  };
+// interface AppState {
+//   counter: number;
+// }
 
-  onIncrement = (): void => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  };
-  onDecrement = (): void => {
-    this.setState({
-      counter: this.state.counter - 1
-    })
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+//   onIncrement = (): void => {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   };
+//   onDecrement = (): void => {
+//     this.setState({
+//       counter: this.state.counter - 1
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     )
+//   }
+// }
+
+const App = (props: AppProps): JSX.Element => {
+  return (
+    <div>{props.color}</div>
+  )
+};
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
