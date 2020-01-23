987fc3829abc850e1b95e3286001ac0270d96c10
yongtany <yongn1494@gmail.com>
2020-01-23 17:36:02 +0900

    Expressing Actions as Type Union

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,27 +1,2 @@
-import axios from 'axios';
-import { Dispatch } from 'redux';
-import { ActionTypes } from './types';
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean
-}
-
-interface FethTodosAction {
-  type: ActionTypes.fetchTodos;
-  payload: Todo[];
-}
-
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-export const fetchTodos = () => {
-  return async (dispatch: Dispatch) => {
-    const response = await axios.get<Todo[]>(url);
-
-    dispatch<FethTodosAction>({
-      type: ActionTypes.fetchTodos,
-      payload: response.data
-    });
-  };
-};
+export * from './todos';
+export * from './types';
---
+++ rrts/src/reducers/todos.ts
@@ -0,0 +1,13 @@
+import { Todo, Action, ActionTypes } from '../actions';
+
+export const todosReducer = (
+  state: Todo[] = [], 
+  action: Action
+) => {
+  switch(action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    default:
+      return state;
+  }
+};
