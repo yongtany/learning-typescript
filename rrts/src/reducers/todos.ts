b795c9aa7687e692222762597247420346cb93d5
yongtany <yongn1494@gmail.com>
2020-01-24 13:18:31 +0900

    Tracking Loading with Component State

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -7,6 +7,8 @@ export const todosReducer = (
   switch(action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload)
     default:
       return state;
   }
